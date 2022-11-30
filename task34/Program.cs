0578b395e4bfdee7bdc9762f95d1e34c37315b9e
Igor From <ig.from@yandex.ru>
2022-11-30 23:11:30 +0700

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] FillArray (int size)
+{
+    int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+return array;
+}
+Console.Write("Введите длинну массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        count++;
+    }
+    }
+    if(count>0)
+    {
+        Console.WriteLine($"В массиве найдено {count} четных чисел");
+    }
+    else
+    {
+    Console.WriteLine("Четных чисел не найдено");
+    }
