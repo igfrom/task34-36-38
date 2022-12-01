f6e05605323a69786b08dbab6b273d49470810b3
Igor From <ig.from@yandex.ru>
2022-12-01 21:38:54 +0700

    add task38

---
+++ task36/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] FillArray(int size)
+{
+    int[] array = new int[size];
+for(int i = 0; i < array.Length; i++ )
+{
+    array[i] = new Random().Next(-10,10);
+}
+return array;
+}
+Console.Write("Введите длинну массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(String.Join(", ", array));
+int sum = 0;
+for (int i = 1; i < array.Length; i+=2)
+{
+    sum = sum + array[i];
+}
+{
+    Console.WriteLine($"Сумма нечетных индексов равна {sum}");
+}
---
+++ task38/Program.cs
@@ -0,0 +1,32 @@
+﻿double[] FillArray(int size)
+{
+double[] array = new double[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+}
+return array;
+}
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = FillArray(size);
+Console.WriteLine (String.Join(", ", array));
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+        
+    
+  if (array[i] < min)
+    {
+         min = array[i];
+    }
+}
+Console.WriteLine($"всего {array.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
